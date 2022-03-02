0d5bdccd14b85bc7116f660372218ccdb29af2c1
Matt <mgstankov30@gmail.com>
2022-03-02 10:25:49 -0500

    add additional page containing url database

--- express_server.js
+++ express_server.js
@@ -12,6 +12,11 @@ app.get('/', (req, res) => {
   res.send('hello');
 });
 
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
