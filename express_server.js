50de399dec5c505fc31a612cb98cbcad9d4c4d33
Matt <mgstankov30@gmail.com>
2022-03-08 13:23:09 -0500

    add login get and render code

--- express_server.js
+++ express_server.js
@@ -46,16 +46,21 @@ const users = {
 // get requests:
 app.get("/register", (req, res) => {
   const templateVars = { 
-    username : req.cookies['username'],
     user : users[req.cookies["user_id"]],
   };
   res.render("urls_register", templateVars);
 });
 
+app.get("/login", (req, res) => {
+  const templateVars = { 
+    user : users[req.cookies["user_id"]],
+  };
+  res.render("url_login", templateVars);
+});
+
 app.get("/urls", (req, res) => {
   const templateVars = { 
     urls : urlDatabase,
-    username : req.cookies['username'],
     user : users[req.cookies["user_id"]],
   };
   console.log(users);
@@ -64,7 +69,6 @@ app.get("/urls", (req, res) => {
 
 app.get("/urls/new", (req, res) => {
   const templateVars = { 
-    username : req.cookies['username'],
     user : users[req.cookies["user_id"]],
   };
   res.render("urls_new", templateVars);
@@ -74,7 +78,6 @@ app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { 
     shortURL: req.params.shortURL, 
     longURL: urlDatabase[req.params.shortURL],
-    username : req.cookies["username"],
     user : users[req.cookies["user_id"]],
   }
   res.render("urls_show", templateVars);
@@ -117,8 +120,8 @@ app.post("/register", (req, res) => {
 });
 
 app.post("/login", (req, res) => {  
-  const username = req.body["username"];
-  res.cookie('username', username);
+  const email = req.body["email"];
+  res.cookie('username', email);
   res.redirect("/urls");
 });
 
