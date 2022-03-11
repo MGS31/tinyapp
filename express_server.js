6b1db680f8b7d081a6498d86fc85fbc3f26df79f
Matt <mgstankov30@gmail.com>
2022-03-10 21:30:53 -0500

    update to req.session cookies instead of cookie parser

--- express_server.js
+++ express_server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(cookieSession({
   name: 'session',
-  keys: [/* secret keys */],
+  keys: ["sadhaskjd"],
 
   // Cookie Options
   maxAge: 24 * 60 * 60 * 1000 // 24 hours
@@ -77,67 +77,70 @@ const urlsForUser = function(id) {
   return userURLS;
 };
 
+
 // get requests:
 app.get("/register", (req, res) => {
   const templateVars = { 
-    user : users[req.cookies["user_id"]],
+    user : users[req.session.user_id],
   };
   res.render("urls_register", templateVars);
 });
 
 app.get("/login", (req, res) => {
   const templateVars = { 
-    user : users[req.cookies["user_id"]],
+    user : users[req.session.user_id],
   };
   res.render("url_login", templateVars);
 });
 
 app.get("/urls", (req, res) => {
-  const userURL = urlsForUser(req.cookies["user_id"]);
+  const userURL = urlsForUser(req.session.user_id);
     const templateVars = {  
       urls : userURL,
-      user : users[req.cookies["user_id"]],
+      user : users[req.session.user_id],
     };
     res.render("urls_index", templateVars)
-})
+});
 
 app.get("/urls/new", (req, res) => {
   const templateVars = { 
-    user : users[req.cookies["user_id"]],
+    user : users[req.session.user_id]
   };
-  if (req.cookies["user_id"]) {
+  if (req.session.user_id) {
+    console.log("Here!");
     res.render("urls_new", templateVars);
   } else {
   res.render("url_login", templateVars);
-  }
+  };
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const userURL = urlsForUser(req.cookies["user_id"]);
+  const userURL = urlsForUser(req.session.user_id);
   const shortURL = req.params.shortURL;
   const longURL = urlDatabase[shortURL].longURL;
   const templateVars = { 
     shortURL: shortURL, 
     longURL: longURL,
-    user : users[req.cookies["user_id"]],
-  }
-  if (!req.cookies["user_id"]) {
+    user : users[req.session.user_id],
+  };
+  if (!req.session.user_id) {
     res.status(400).send("Unauthorised Request");
-  }
-  if (req.cookies["user_id"] === userURL) {
+  };
+  if (req.session.user_id === userURL[shortURL].userID) {
     res.render("urls_show", templateVars);
   } else {
+    console.log("Test: ", urlsForUser(req.session.user_id));
     res.status(400).send("Unauthorised Request");
-  }
+  };
 });
 
 app.get ("/urls/:shortURL/edit", (req, res) => {
   const shortURL = req.params.shortURL;
-  if (req.cookies["user_id"]) {
+  if (req.session.user_id) {
   res.redirect(`/urls/${shortURL}`);
   } else {
   return res.status(400).send("Unauthorised Request");
-}
+};
 });
 
 app.get("/u/:shortURL", (req, res) => {
@@ -147,7 +150,7 @@ app.get("/u/:shortURL", (req, res) => {
     return res.status(400).send("Unauthorised Request");
   } else {
     res.redirect(longURL);
-  }
+  };
 });
 
 app.get("/urls.json", (req, res) => {
@@ -164,70 +167,71 @@ app.post("/register", (req, res) => {
   if (email !== '' && password !== '') {
     if (!checkEmail(email, users)) {
       users[id] = { id, email, password : hashedPassword };
-      console.log(users);
-      res.cookie("user_ID", id);
+      req.session.user_id = id;
       res.redirect(`/urls`);
     } else {
       return res.status(400).send("Invalid Credentials!");
-    }
+    };
   } else {
     return res.status(400).send("Invalid Credentials!");
-  }
+  };
 });
 
 app.post("/login", (req, res) => {  
   const inputEmail = req.body.email;
   const inputPass = req.body.password;
-
   if (!inputEmail || !inputPass) {
-    return res.status(403).send("Invalid Credentials")
-  }
+    return res.status(403).send("Invalid Credentials!")
+  };
   const userID = getUserByEmail(inputEmail);
   const password = userID.password;
   if (!userID) {
-    return res.status(403).send("Invalid Credentials");
-  }
+    return res.status(403).send("Invalid Credentials!");
+  };
   if (!bcrypt.compareSync(inputPass, password)) {
-    return res.status(403).send("Invalid Credentials");
-  }
+    return res.status(403).send("Invalid Credentials!");
+  };
   const user_id = userID.id;
-  res.cookie("user_id", user_id);
+  req.session.user_id = user_id;
   res.redirect("/urls");
-
 });
 
 app.post("/logout", (req, res) => {  
+  // set session to null
   res.clearCookie("user_id");
   res.redirect("/urls");
 });
 
 app.post("/urls", (req, res) => {
-  if (req.cookies["user_id"]) {
+  if (req.session.user_id) {
   const shortURL = generateRandomString(6);
   const longURL = req.body["longURL"];
-  const userID = req.cookies["user_id"];
+  const userID = req.session.user_id;
   urlDatabase[shortURL] = { longURL, userID };
   res.redirect(`/urls/${shortURL}`);
   } else {
     return res.status(400).send("Unauthorised Request");
-  }
+  };
 });
 
-app.post("/urls/:id", (req, res) => {
-  const shortURL = req.params.id;
+app.post("/urls/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
   const longURL = req.body["longURL"];
-  urlDatabase[shortURL] = longURL;
-  res.redirect("/urls")
-})
+  if (req.session.user_id) {
+  urlDatabase[shortURL].longURL = longURL;
+  } else {
+    return res.status(400).send("Unauthorised Request");
+  };
+});
 
 app.post("/urls/:shortURL/delete", (req, res) => {
-  if (req.cookies["user_id"]) {
+  if (req.session.user_id) {
   const shortURL = req.params.shortURL;
   delete urlDatabase[shortURL];
   res.redirect("/urls");
 } else {
   return res.status(400).send("Unauthorised Request");
-}
+};
 });
 
 // port listening confirmation
